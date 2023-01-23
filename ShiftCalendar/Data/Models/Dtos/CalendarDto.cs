12cf2266a51821d7c39fe06d4d7b94a28072d982
Ramon Silvente Galera <rsilgal@gmail.com>
2023-01-23 16:06:45 +0100

    [ADD] Calendar UI and CalendarDto

---
+++ ShiftCalendar/Data/Models/Dtos/CalendarDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShiftCalendar.Data.Models.Dtos
+{
+    public class CalendarDto
+    {
+        [Required, MinLength(2)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
