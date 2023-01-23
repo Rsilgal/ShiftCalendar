0f6948f7843ae8411fef63ba5712c72fd418af94
Ramon Silvente Galera <rsilgal@gmail.com>
2023-01-23 16:07:15 +0100

    [ADD] Secuency UI and SecuencyDto

---
+++ ShiftCalendar/Data/Models/Dtos/SecuencyDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShiftCalendar.Data.Models.Dtos
+{
+    public class SecuencyDto
+    {
+        [Required, MinLength(2)]
+        public string Name { get; set; } = string.Empty;
+        [Required, MinLength(5)]
+        public string Description { get; set; } = string.Empty;
+        [Required]
+        public CalendarModel Calendar { get; set; }
+        [Required]
+        public ICollection<ShiftModel> Shift { get; set; }
+    }
+}
