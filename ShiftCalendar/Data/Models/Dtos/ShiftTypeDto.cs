44ecb00ef7fa1ff46eb669e31ae77b05fcdbd6d3
Ramon Silvente Galera <rsilgal@gmail.com>
2023-01-23 16:07:48 +0100

    [ADD] ShiftType UI and ShiftTypeDto

---
+++ ShiftCalendar/Data/Models/Dtos/ShiftTypeDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShiftCalendar.Data.Models.Dtos
+{
+    public class ShiftTypeDto
+    {
+        [Required, MinLength(1)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, MinLength(5)]
+        public string Description { get; set; } = string.Empty;
+    }
+}
