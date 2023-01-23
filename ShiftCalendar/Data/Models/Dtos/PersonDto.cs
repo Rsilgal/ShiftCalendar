39c7d398ac0f2a01c800a47d7e61a7bc0b810999
Ramon Silvente Galera <rsilgal@gmail.com>
2023-01-23 16:06:59 +0100

    [ADD] Person UI and PersonDto

---
+++ ShiftCalendar/Data/Models/Dtos/PersonDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShiftCalendar.Data.Models.Dtos
+{
+    public class PersonDto
+    {
+        [Required, MinLength(5)]
+        public string FirstName { get; set; }
+        [Required, MinLength(5)]
+        public string LastName { get; set; }
+        [Required]
+        public ShiftModel Shift { get; set; }
+    }
+}
